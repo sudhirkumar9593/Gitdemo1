2cb6f112034976054775896a8b5beee2e0d4fa0c
sudhir <kumarsudhir131@yahoo.in>
2021-04-09 23:38:12 +0530

    second commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+		
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAUtomtion() {
+		System.out.println("BrowserAUtomtion");
+	
+	}
+	
+	@Test
+	public void elementsUi() {
+		System.out.println("elementsUi");
+	}
+}
