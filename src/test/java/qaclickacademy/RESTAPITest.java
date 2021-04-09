b92120915ca53c8fe19c7465362d4c23ef4ba19c
sudhir <kumarsudhir131@yahoo.in>
2021-04-10 01:13:04 +0530

    updting from the gitX

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -6,6 +6,11 @@ public class RESTAPITest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira1");
+		
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		System.out.println("postJira4");
 		
 	}
 
